1a353ac632e2b4310bfd0dc2af7c5e435dab92a7
alicejx18 <109005602+alicejx18@users.noreply.github.com>
2022-07-15 15:47:04 -0400

    Update script.js

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -49,6 +49,7 @@ WARNING!!! WARNING!!!
   // in the loop. Otherwise, call helloSpeaker's 'speak' method with the current
   // name in the loop.
  
+(function () {
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
@@ -63,4 +64,3 @@ var firstLetter = names[i].charAt(0).toLowerCase();
 })();
 
 
-(function () {
