8a87d32e13bc22847dea974228e5a396cf79f76d
alicejx18 <109005602+alicejx18@users.noreply.github.com>
2022-07-15 15:55:00 -0400

    Update SpeakHello.js

--- mod4-solution/SpeakHello.js
+++ mod4-solution/SpeakHello.js
@@ -23,8 +23,8 @@
 
 (function (window) {
   var speakWord = "Hello";
-  var helloSpeaker.speak = function (name) {
-      console.log(speakWord + " " + name);
+  var helloSpeaker.speak = function (names) {
+      console.log(speakWord + " " + names);
   }
     
     window.helloSpeaker = helloSpeaker;
