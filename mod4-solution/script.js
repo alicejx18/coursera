d5df5150b37aeea6112fdae2ef511ee8359f77c8
alicejx18 <109005602+alicejx18@users.noreply.github.com>
2022-07-15 15:53:21 -0400

    Update script.js

--- mod4-solution/script.js
+++ mod4-solution/script.js
@@ -53,10 +53,11 @@ WARNING!!! WARNING!!!
 var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
-var firstLetter = names[i].charAt(0).toLowerCase();
- if (firstLetter === 'j') {
+  var firstLetter = names[i].charAt(0).toLowerCase();
+  if (firstLetter === 'j') {
     byeSpeaker.speak(names[i]);
-  } else {
+  } 
+  else {
     helloSpeaker.speak(names[i]);
   }
 }
